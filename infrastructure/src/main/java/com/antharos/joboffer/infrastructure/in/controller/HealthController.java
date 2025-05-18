dfef19c5fe76f8b9493679ee8381fbd9f3c16c7f
Miguel García de Prada <miguelgardep@uoc.edu>
2025-05-18 22:17:19 +0200

    #6 Deploy on Azure

---
+++ infrastructure/src/main/java/com/antharos/joboffer/infrastructure/in/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.antharos.joboffer.infrastructure.in.controller;
+
+import jakarta.annotation.security.PermitAll;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+  @PermitAll
+  @GetMapping("/health")
+  public ResponseEntity<Map<String, String>> health() {
+    Map<String, String> status = new HashMap<>();
+    status.put("status", "ok");
+    return ResponseEntity.ok(status);
+  }
+}
